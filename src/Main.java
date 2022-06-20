51f3fd81ac827d9598785f316e227f6f9468ecd2
cioloca535 <cioloca535@gmail.com>
2022-06-20 09:13:45 -0700

    Primul commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Introduceti o propozitie:");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(scanner.nextLine());
+
+    }
+
+}
